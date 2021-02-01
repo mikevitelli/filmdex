8a210bf5c29547ccd30884a28e9b6fbe53605a11
Mike <mike@mikevitelli.com>
2021-02-01 17:44:11 -0500

    refactor

--- components/FilmCard/index.js
+++ components/FilmCard/index.js
@@ -1,38 +1,9 @@
 import styles from "../../styles/Home.module.css";
 import Image from "next/image";
 import React, { useState } from "react";
-import { Modal, Container, Row, Col, Button } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import FilmModal from "../FilmModal";
 
-import "./Card.module.css";
-// MODAL
-function FilmModal(props) {
-  return (
-    <Modal
-      {...props}
-      size="sm"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Container
-        style={{
-          justifyContent: "center",
-          alignItems: "center",
-          textAlign: "center",
-        }}
-      >
-        <Row>
-          <Col style={{ paddingTop: "15px", paddingBottom: "15px" }}>
-            <Image src="/film_guy512.png" width={200} height={200} />
-            <h3>name</h3>
-            <h2>description</h2>
-          </Col>
-        </Row>
-      </Container>
-    </Modal>
-  );
-}
-
-// CARD
 const FilmCard = (props) => {
   const [modalShow, setModalShow] = useState(false);
   return (
---
+++ components/FilmModal/index.js
@@ -0,0 +1,26 @@
+import { Modal, Container, Row, Col } from "react-bootstrap";
+import Image from "next/image";
+import styles from "../../styles/Home.module.css";
+
+const FilmModal = (props) => {
+  return (
+    <Modal
+      {...props}
+      size="sm"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Container className={styles.filmContainer}>
+        <Row>
+          <Col className={styles.filmCol}>
+            <Image src="/film_guy512.png" width={200} height={200} />
+            <h3>name</h3>
+            <h2>description</h2>
+          </Col>
+        </Row>
+      </Container>
+    </Modal>
+  );
+};
+
+export default FilmModal;
