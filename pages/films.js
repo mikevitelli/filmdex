8d8284d0ecf490d55e54dd7f253fc3829d1086a4
Mike <mike@mikevitelli.com>
2021-02-12 17:22:57 -0500

    added and styled search bar component

--- pages/films.js
+++ pages/films.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 
 import styles from "../styles/Home.module.css";
 import FilmCards from "../components/FilmCards";
+import SearchBar from "material-ui-search-bar";
 // // TO USE DATABASE DIRECTLY
 // import { connectToDatabase } from "../util/mongodb";
 // export async function getStaticProps() {
@@ -31,6 +32,8 @@ export async function getStaticProps() {
   };
 }
 
+[];
+
 const Films = (props) => {
   return (
     <>
@@ -40,12 +43,19 @@ const Films = (props) => {
       </Head>
       <div className={styles.container}>
         {/* HEADER */}
-        <h1 className={styles.title} style={{ marginTop: "30px" }}>
+        <h1
+          className={styles.title}
+          style={{ marginTop: "30px", marginBottom: "30px" }}
+        >
           <a href="/">Films</a>
         </h1>
-
+        <SearchBar
+          className={styles.searchBar}
+          // value={state.value}
+          // onChange={(newValue) => setState({ value: newValue })}
+          // onRequestSearch={() => doSomethingWith(state.value)}
+        />
         {/* GRID COMPONENT */}
-
         <FilmCards {...props} />
       </div>
     </>
