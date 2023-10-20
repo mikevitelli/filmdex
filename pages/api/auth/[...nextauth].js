66398176bc4e0b959d01271a4e1f448a8dab0929
Mike Vitelli <mike@mikevitelli.com>
2023-10-20 18:28:20 -0400

    fixed url

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -30,7 +30,7 @@ export default NextAuth({
           const userExists = await User.findOne({ email });
 
           if (!userExists) {
-            const res = await fetch("http://localhost:3000/api/user", {
+            const res = await fetch("https://filmdex.app/api/user", {
               method: "POST",
               headers: {
                 "Content-Type": "application/json",
